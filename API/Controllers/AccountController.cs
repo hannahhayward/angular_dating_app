4f2c09ef16898b786fe9af8fc733c69244867ebc
Hannah Hayward <hannah.hayward@allata.com>
2021-11-02 14:40:39 -0600

    Passing postman tests for registering

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -22,16 +23,22 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+        if (await UserExists(registerDto.Username)) return BadRequest("username is taken");
+
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
-                Username = registerDto.Username,
+                Username = registerDto.Username.ToLower(),
                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
             return user;
+        }
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.Username == username.ToLower());
         }
     }
 }
