e0a48545e39359234e38b44fd1b09e5ce47f108c
Hannah Hayward <hannah.hayward@allata.com>
2021-11-08 10:22:41 -0700

    created and conditionally rendering home page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
