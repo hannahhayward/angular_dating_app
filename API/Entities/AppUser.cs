106a9812ad6561748ec701c1514b9dd3d66624cf
Hannah Hayward <hannah.hayward@allata.com>
2021-11-12 15:39:29 -0700

    created calculate age extension method

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Extensions;
 
 namespace API.Entities
 {
@@ -11,8 +12,8 @@ namespace API.Entities
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
- public DateTime DateOfBirth { get; set; }       
- public string KnownAs { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime LastActive { get; set; } = DateTime.Now;
         public string Gender { get; set; }
@@ -22,5 +23,9 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
