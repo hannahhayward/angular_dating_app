2d045d5143d69b58620cd238e8667642b3e17e8f
Hannah Hayward <hannah.hayward@allata.com>
2021-11-17 14:58:30 -0700

    created repository and added methods

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -10,30 +10,29 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+    [Authorize]
     public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
-        public UsersController( DataContext context)
+        public UsersController(DataContext context)
         {
             _context = context;
         }
-        [AllowAnonymous ]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-          return await _context.Users.ToListAsync();
+            return await _context.Users.ToListAsync();
         }
-        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-           return await _context.Users.FindAsync(id);
+            return await _context.Users.FindAsync(id);
         }
         // [HttpDelete("{id}")]
         // [Authorize]
         // public async Task<ActionResult<AppUser>> DeleteUser(int id)
         // {
-        
+
         // } 
     }
 }
