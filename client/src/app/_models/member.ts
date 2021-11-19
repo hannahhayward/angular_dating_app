9241ce082849923d4571f82139c3fcd9ecea1917
Hannah Hayward <hannah.hayward@allata.com>
2021-11-19 13:43:54 -0700

    added get methods to service

--- client/src/app/_models/member.ts
+++ client/src/app/_models/member.ts
@@ -1,25 +1,23 @@
 import { Photo } from "./photo";
 
-declare module namespace {
 
 
 
-    export interface Member {
-        id: number;
-        username: string;
-        photoUrl: string;
-        age: number;
-        knownAs: string;
-        createdAt: Date;
-        lastActive: Date;
-        gender: string;
-        introduction: string;
-        lookingFor: string;
-        interests: string;
-        city: string;
-        country: string;
-        photos: Photo[];
-    }
-
+export interface Member {
+    id: number;
+    username: string;
+    photoUrl: string;
+    age: number;
+    knownAs: string;
+    createdAt: Date;
+    lastActive: Date;
+    gender: string;
+    introduction: string;
+    lookingFor: string;
+    interests: string;
+    city: string;
+    country: string;
+    photos: Photo[];
 }
+
 
