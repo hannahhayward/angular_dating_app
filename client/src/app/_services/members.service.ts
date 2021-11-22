3937d562436adc723078dc14aadadd7135c7f1ca
Hannah Hayward <hannah.hayward@allata.com>
2021-11-22 16:18:14 -0700

    jwt interceptor added and functioning

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -4,11 +4,7 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
-  })
-}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -17,9 +13,9 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
   getMember(username) {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, httpOptions);
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 }
