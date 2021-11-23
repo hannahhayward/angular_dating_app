131d6e072049de90284b8b305c3cbe1c2a403571
Hannah Hayward <hannah.hayward@allata.com>
2021-11-23 08:47:55 -0700

    member details page is working

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -14,7 +14,7 @@ export class MembersService {
   getMembers() {
     return this.http.get<Member[]>(this.baseUrl + 'users');
   }
-  getMember(username) {
+  getMember(username: string) {
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 }
