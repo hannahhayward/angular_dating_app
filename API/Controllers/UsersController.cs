7d6176ffe5b6d11c2fb077e96b3b2618aa1b7585
Hannah Hayward <hannah.hayward@allata.com>
2021-12-01 11:06:13 -0700

    test

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -86,19 +86,21 @@ namespace API.Controllers
             if (await _userRepository.SaveAllAsync()) return NoContent();
             return BadRequest("failed to set main photo");
         }
+
         [HttpDelete("delete-photo/{photoId}")]
-        public async Task<ActionResult> DeletePhoto(int photoId){
+        public async Task<ActionResult> DeletePhoto(int photoId)
+        {
             var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
             var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
-            if(photo == null) return NotFound();
-            if(photo.IsMain) return BadRequest("cannot delete main photo");
-            if(photo.PublicId != null)
+            if (photo == null) return NotFound();
+            if (photo.IsMain) return BadRequest("cannot delete main photo");
+            if (photo.PublicId != null)
             {
                 var result = await _photoService.DeletePhotoAsync(photo.PublicId);
-                if(result.Error != null)return BadRequest(result.Error.Message);
+                if (result.Error != null) return BadRequest(result.Error.Message);
             }
             user.Photos.Remove(photo);
-            if(await _userRepository.SaveAllAsync()) return Ok();
+            if (await _userRepository.SaveAllAsync()) return Ok();
             return BadRequest("failed to delete the photo");
         }
     }
