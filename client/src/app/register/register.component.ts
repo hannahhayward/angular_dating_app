c43ab2c59ca3cb34b978e24ef3f7ad30628c95f1
Hannah Hayward <hannah.hayward@allata.com>
2021-12-01 12:02:04 -0700

    basic set up of reactive form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
@@ -11,19 +12,29 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter()
   model: any = {};
+  registerForm: FormGroup;
   constructor(private accountService: AccountService, public toastr: ToastrService) { }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   ngOnInit(): void {
+    this.initializeForm();
+  }
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    })
   }
 
   register() {
-    this.accountService.register(this.model).subscribe(response => {
-      console.log(response);
-      this.cancel();
-    }, error => {
-      this.toastr.error(error.error);
-    })
+    // this.accountService.register(this.model).subscribe(response => {
+    //   console.log(response);
+    //   this.cancel();
+    // }, error => {
+    //   this.toastr.error(error.error);
+    // })
+    console.log(this.registerForm.value);
   }
   cancel() {
     this.cancelRegister.emit(false);
