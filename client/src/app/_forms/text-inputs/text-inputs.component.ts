91d6e7ad5a35c9babb5c55534fc0ba61f2172ccd
Hannah Hayward <hannah.hayward@allata.com>
2021-12-08 10:22:27 -0700

    reusable text input forms created and working

--- client/src/app/_forms/text-inputs/text-inputs.component.ts
+++ client/src/app/_forms/text-inputs/text-inputs.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Self } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
 
 @Component({
   selector: 'app-text-inputs',
   templateUrl: './text-inputs.component.html',
   styleUrls: ['./text-inputs.component.css']
 })
-export class TextInputsComponent implements OnInit {
+export class TextInputsComponent implements ControlValueAccessor {
+  @Input() label: string;
+  @Input() type = 'text';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(@Self() public ngControl: NgControl) {
+    this.ngControl.valueAccessor = this;
+   }
+  writeValue(obj: any): void {
+  }
+  registerOnChange(fn: any): void {
+  }
+  registerOnTouched(fn: any): void {
   }
 
 }
