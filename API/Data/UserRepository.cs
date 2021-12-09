6aedc6e62c1de2136ac05b4310e4670784bd4bd1
Hannah Hayward <hannah.hayward@allata.com>
2021-12-09 13:19:47 -0700

    added min/max age filters

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -27,6 +27,10 @@ namespace API.Data
             var query = _context.Users.AsQueryable();
             query = query.Where(u => u.Username != userParams.CurrentUsername);
             query = query.Where(u => u.Gender == userParams.Gender);
+
+            var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+            var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+            query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
             return await PagedList<MemberDto>.CreateAsync(query.ProjectTo<MemberDto>(_mapper
             .ConfigurationProvider).AsNoTracking(),
             userParams.PageNumber, userParams.PageSize);
