5a7f2babe12094b6c20aa631f07b3172eb956a83
Hannah Hayward <hannah.hayward@allata.com>
2021-12-10 13:19:10 -0700

    now caching with filters and sorting

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { of, pipe } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
@@ -14,22 +14,33 @@ import { UserParams } from '../_models/userParams';
 export class MembersService {
   baseUrl = environment.apiUrl
   members: Member[] = [];
+  memberCache = new Map();
+
   constructor(private http: HttpClient) { }
 
   getMembers(userParams: UserParams) {
+    var response = this.memberCache.get(Object.values(userParams).join('-'));
+    if (response) {
+      return of(response);
+    }
+
     let params = this.getPaginationHeaders(userParams.pageNumber, userParams.pageSize);
     params = params.append('minAge', userParams.minAge.toString());
     params = params.append('maxAge', userParams.maxAge.toString());
     params = params.append('gender', userParams.gender);
     params = params.append('orderBy', userParams.orderBy);
-    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users',params)
+
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params)
+    .pipe(map(response => {
+      this, this.memberCache.set(Object.values(userParams).join('-'), response);
+      return response;
+    }))
   }
-
-
 
   getMember(username: string) {
     const member = this.members.find(x => x.username === username);
     if (member !== undefined) return of(member);
+
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
   updateMember(member: Member) {
