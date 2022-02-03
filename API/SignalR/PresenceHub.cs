0a9cadbeb00fa31d751af1816dbd3fe59b4f648a
Hannah Hayward <hannah.hayward@allata.com>
2022-02-03 11:20:04 -0700

    updated presence hub connectoin method to access dictionary

--- API/SignalR/PresenceHub.cs
+++ API/SignalR/PresenceHub.cs
@@ -10,10 +10,20 @@ namespace API.SignalR
 {
     public class PresenceHub : Hub
     {
+        private readonly PresenceTracker _tracker;
+        public PresenceHub(PresenceTracker tracker)
+        {
+            _tracker = tracker;
+        }
+
         [Authorize]
         public override async Task OnConnectedAsync()
         {
+            await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
             await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+
+            var currentUsers = await _tracker.GetOnlineUsers();
+            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
